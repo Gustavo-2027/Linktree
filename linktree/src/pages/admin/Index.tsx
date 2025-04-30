ed1eaf44d337a788280c938d2a9fa04ffe389080
Gustavo Silva <GustavoNas.27@gmail.com>
2025-04-30 00:49:57 -0300

    Criando e configurando form da pag de admin

--- linktree/src/pages/admin/Index.tsx
+++ linktree/src/pages/admin/Index.tsx
@@ -1,12 +1,79 @@
-import Header from "../../components/header/Index"
-
+import { useState } from "react";
+import Header from "../../components/header/Index";
+import Input from "../../components/social/input/Index";
 
 const Admin = () => {
-  return (
-    <div className="w-full min-h-dvh bg-gradient-to-r from-zinc-500 via-zinc-600 to-zinc-800 text-white flex flex-col items-center text-center py-7 px-2">
-      <Header/>
-    </div>
-  )
-}
+  const [nameInput, setNameInput] = useState("");
+  const [urlInput, setUrlInput] = useState("");
+  const [textColorInput, setTextColorInput] = useState("#f1f1f1");
+  const [backgroundColorInput, setBackgroundColorInput] = useState("#00000");
 
-export default Admin
+  return (
+    <div className="w-full min-h-dvh bg-gradient-to-r from-zinc-500 via-zinc-600 to-zinc-800 text-white flex flex-col items-center py-7 px-2">
+      <Header />
+      <form className="flex flex-col my-8 w-full max-w-xl">
+        <label htmlFor="inome" className="text-white font-medium mt-2 mb-3">
+          Nome do Link
+        </label>
+        <Input
+          placeholder="Digite o nome do Link"
+          id="inome"
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+        />
+
+        <label htmlFor="iurl" className="text-white font-medium mt-2 mb-3">
+          URL do Link
+        </label>
+        <Input
+          type="url"
+          placeholder="Digite a URL"
+          id="iurl"
+          value={urlInput}
+          onChange={(e) => setUrlInput(e.target.value)}
+        />
+
+        <section className="flex gap-10 my-4 ">
+          <div className="flex gap-2">
+            <label
+              htmlFor="itextFundo"
+              className="text-white font-medium mt-2 mb-3"
+            >
+              Cor do Link
+            </label>
+            <input
+              type="color"
+              value={textColorInput}
+              onChange={(e) => setTextColorInput(e.target.value)}
+              id="itextFundo"
+            />
+          </div>
+          <div className="flex gap-2">
+            <label
+              htmlFor="ifundo"
+              className="text-white font-medium mt-2 mb-3"
+            >
+              Fundo do Link
+            </label>
+            <input
+              type="color"
+              value={backgroundColorInput}
+              onChange={(e) => setBackgroundColorInput(e.target.value)}
+              id="ifundo"
+            />
+          </div>
+        </section>
+        <div className="flex flex-col justify-start items-center mb-7 p-1 border-gray-100/25 border-1 rounded-md w-full text-center">
+          <label htmlFor="" className="text-white font-medium mt-2 mb-3">
+            Veja como está ficando: 
+          <article className="w-11/12 max-w-lg flex flex-col items-center justify-between rounded px-1 py-3 " style={{marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput}}>
+            <p style={{color: textColorInput}} className="font-medium">{nameInput}</p>
+          </article>
+          </label>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Admin;
